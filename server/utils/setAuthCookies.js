72f187aeba4a490d4a1a2ebbe8f5a03ade292c2a
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-11-05 18:34:44 +0530

    sameSite none for cookies

--- server/utils/setAuthCookies.js
+++ server/utils/setAuthCookies.js
@@ -4,12 +4,14 @@ export default async (res, { accessToken, refreshToken }) => {
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
     secure: env.NODE_ENV === 'production',
+    sameSite: 'none',
     maxAge: 36_00_000,
   });
 
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     secure: env.NODE_ENV === 'production',
+    sameSite: 'none',
     maxAge: 60_48_00_000,
   });
 };
